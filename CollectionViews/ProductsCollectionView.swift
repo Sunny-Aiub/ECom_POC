a8eedf3354b0207d54d74dd9176ebffeebc30e30
Md. Mahadhi Hassan Chowdhury <Sunny@Dream71s-iMac.local>
2018-11-12 15:04:30 +0600

    first commit

---
+++ CollectionViews/ProductsCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  ProductsCollectionView.swift
+//  PocEcomHome
+//
+//  Created by Md. Mahadhi Hassan Chowdhury on 11/12/18.
+//  Copyright © 2018 Md. Mahadhi Hassan Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class ProductsCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TableViews/HomeTable.swift
@@ -0,0 +1,21 @@
+//
+//  HomeTable.swift
+//  PocEcomHome
+//
+//  Created by Md. Mahadhi Hassan Chowdhury on 11/12/18.
+//  Copyright © 2018 Md. Mahadhi Hassan Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class HomeTable: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
